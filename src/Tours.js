745207f98389f844f7169b71e42dbce9aa133b05
Mikhail Pasichniuk <michael.reynmar@gmail.com>
2021-10-12 17:13:05 +0300

    save working tours with pure js and props

---
+++ src/Tours.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours, removeTour }) => {
+    return (
+        <section>
+            <div className="title">
+                <h2>Наши туры</h2>
+                <div className="underline"></div>
+            </div>
+            <div className="mainContent">
+                {tours.map((tour) => {
+                    return (
+                        <Tour
+                            key={tour.id}
+                            {...tour}
+                            removeTour={removeTour}
+                        ></Tour>
+                    );
+                })}
+            </div>
+        </section>
+    );
+};
+
+export default Tours;
