60f5ae30c61b454b844e29392ccc42c4e23520a8
Mikhail Pasichniuk <michael.reynmar@gmail.com>
2021-10-13 13:34:41 +0300

    Updates

---
+++ precache-manifest.7f54abfdb698a1ab62d3401bdc4855b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c611abe5dadf41f2e59d1611513759",
+    "url": "/react-alfa-test/index.html"
+  },
+  {
+    "revision": "67f47486b5bc98400edc",
+    "url": "/react-alfa-test/static/css/main.a895fa02.chunk.css"
+  },
+  {
+    "revision": "8ea2c6101239c0e1f2cb",
+    "url": "/react-alfa-test/static/js/2.769fdb1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-alfa-test/static/js/2.769fdb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f47486b5bc98400edc",
+    "url": "/react-alfa-test/static/js/main.59a024cb.chunk.js"
+  },
+  {
+    "revision": "d60af3463750a309e166",
+    "url": "/react-alfa-test/static/js/runtime-main.f035804c.js"
+  }
+]);
